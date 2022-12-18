b018d9d13d3ccc8cd1a079d165df5f22d63f5bae
Babayaga <108718751+Anjaneyuluu@users.noreply.github.com>
2022-12-19 01:11:21 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,18 +1,6 @@
-def call(' string defaultValue:,description:,name:'){
-  parameters {
-        string defaultValue: '11', description: 'Enter number one', name: 'varOne'
-        string defaultValue: '22', description: 'Enter number Two', name: 'varTwo'
-    }
-    stages {
-        stage("Add Two Numbers") {
-            steps {
-                script {
-                    def varOne = params.varOne as int
-                    def varTwo = params.varTwo as int
-                    def result = varOne + varTwo
-                    echo "result is ${result}"
-                }
-            }
-        }
-    }
+def examFun(String[] val){
+  return [
+     new randClass(name: val[0], place: 'London')
+     new randClass(name: val[1], place: 'Berlin')
+  ]
 }
