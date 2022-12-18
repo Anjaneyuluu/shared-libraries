6ae8241c8278033c5eb25d73bb8be2806202aa69
Babayaga <108718751+Anjaneyuluu@users.noreply.github.com>
2022-12-19 01:03:52 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,2 +1,18 @@
-def call(''){
+def call(' string defaultValue:,description:,name:'){
+  parameters {
+        string defaultValue: '11', description: 'Enter number one', name: 'varOne'
+        string defaultValue: '22', description: 'Enter number Two', name: 'varTwo'
+    }
+    stages {
+        stage("Add Two Numbers") {
+            steps {
+                script {
+                    def varOne = params.varOne as int
+                    def varTwo = params.varTwo as int
+                    def result = varOne + varTwo
+                    echo "result is ${result}"
+                }
+            }
+        }
+    }
 }
